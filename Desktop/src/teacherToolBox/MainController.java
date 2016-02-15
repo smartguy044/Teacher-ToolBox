e6cae3715f1dee031e1c5b70eb47fd662113d70e
smartguy044 <mdstevens2@my.waketech.edu>
2016-02-15 16:22:27 -0500

    Misc adjustments for top bar UI

--- Desktop/src/teacherToolBox/MainController.java
+++ Desktop/src/teacherToolBox/MainController.java
@@ -13,8 +13,6 @@ import javafx.scene.layout.StackPane;
 
 import javax.annotation.PostConstruct;
 
-import com.jfoenix.controls.JFXDrawer;
-import com.jfoenix.controls.JFXHamburger;
 import com.jfoenix.controls.JFXPopup;
 import com.jfoenix.controls.JFXPopup.PopupHPosition;
 import com.jfoenix.controls.JFXPopup.PopupVPosition;
@@ -29,24 +27,15 @@ public class MainController
 
     @FXML
     private StackPane root;
+
     @FXML
     private StackPane content;
-    @FXML
-    private StackPane sideContent;
-
-    @FXML
-    private StackPane titleBurgerContainer;
-
-    @FXML
-    private JFXHamburger titleBurger;
 
     @FXML
     private StackPane optionsBurger;
-    @FXML
-    private JFXRippler optionsRippler;
 
     @FXML
-    private JFXDrawer drawer;
+    private JFXRippler optionsRippler;
 
     @FXML
     private JFXPopup toolbarPopup;
@@ -55,44 +44,15 @@ public class MainController
     private Label exit;
 
     private FlowHandler flowHandler;
-    private FlowHandler sideMenuFlowHandler;
-
-    private int counter = 0;
 
     @PostConstruct
     public void init() throws FlowException, VetoException
     {
-        // init the title hamburger icon
-        drawer.setOnDrawingAction((e) -> {
-            titleBurger.getAnimation().setRate(1);
-            titleBurger.getAnimation().setOnFinished((event) -> counter = 1);
-            titleBurger.getAnimation().play();
-        });
-
-        drawer.setOnHidingAction((e) -> {
-            titleBurger.getAnimation().setRate(-1);
-            titleBurger.getAnimation().setOnFinished((event) -> counter = 0);
-            titleBurger.getAnimation().play();
-        });
-
-        titleBurgerContainer.setOnMouseClicked((e) -> {
-            if (counter == 0)
-            {
-                drawer.draw();
-            }
-            else if (counter == 1)
-            {
-                drawer.hide();
-            }
-            counter = -1;
-        });
-
         // init Popup
         toolbarPopup.setPopupContainer(root);
         toolbarPopup.setSource(optionsRippler);
-        optionsBurger.setOnMouseClicked((e) -> {
-            toolbarPopup.show(PopupVPosition.TOP, PopupHPosition.RIGHT, -12, 15);
-        });
+
+        optionsBurger.setOnMouseClicked((e) -> toolbarPopup.show(PopupVPosition.TOP, PopupHPosition.RIGHT, -12, 15));
 
         // close application
         exit.setOnMouseClicked((e) -> Platform.exit());
@@ -108,12 +68,6 @@ public class MainController
         context.register("ContentFlow", innerFlow);
         context.register("ContentPane", content);
         content.getChildren().add(flowHandler.start(new AnimatedFlowContainer(Duration.millis(320), ContainerAnimations.SWIPE_LEFT)));*/
-
-        // side controller will add links to the content flow
-        /*Flow sideMenuFlow = new Flow(SideMenuController.class);
-        sideMenuFlowHandler = sideMenuFlow.createHandler(context);
-        sideContent.getChildren().add(sideMenuFlowHandler.start(new AnimatedFlowContainer(Duration.millis(320),
-        ContainerAnimations.SWIPE_LEFT)));*/
 
     }
 }
