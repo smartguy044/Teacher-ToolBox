c309f55748ded3f4f18e1b9e69fe404f084351b1
smartguy044 <mdstevens2@my.waketech.edu>
2016-03-09 10:43:19 -0500

    Update Add/Edit/Delete student menu

---
+++ Desktop/src/teacherToolBox/controller/AddStudentController.java
@@ -0,0 +1,19 @@
+package teacherToolBox.controller;
+
+import com.jfoenix.controls.JFXDecorator;
+import io.datafx.controller.FXMLController;
+import io.datafx.controller.flow.Flow;
+import io.datafx.controller.flow.container.DefaultFlowContainer;
+import io.datafx.controller.flow.context.FXMLViewFlowContext;
+import io.datafx.controller.flow.context.ViewFlowContext;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import teacherToolBox.Main;
+import teacherToolBox.maincontroller.MainAddAssignmentController;
+
+@FXMLController("../fxml/AddStudent.fxml")
+
+public class AddStudentController
+{
+
+}
