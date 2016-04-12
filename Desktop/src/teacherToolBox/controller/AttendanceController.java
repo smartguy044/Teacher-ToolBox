e3fc1e9a00126977dcbb7aa5fc49763b9569edeb
smartguy044 <mdstevens2@my.waketech.edu>
2016-04-12 09:01:25 -0400

    Connect Attendance Controller to dB

--- Desktop/src/teacherToolBox/controller/AttendanceController.java
+++ Desktop/src/teacherToolBox/controller/AttendanceController.java
@@ -5,20 +5,18 @@ import com.jfoenix.controls.JFXComboBox;
 import io.datafx.controller.FXMLController;
 import io.datafx.controller.flow.action.ActionMethod;
 import io.datafx.controller.flow.action.ActionTrigger;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import teacherToolBox.components.Student;
 
 import javax.annotation.PostConstruct;
-import javax.swing.text.TabableView;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.*;
 import java.text.SimpleDateFormat;
 import java.util.*;
-import java.util.Date;
 
 @FXMLController("../fxml/Attendance.fxml")
 
@@ -37,16 +35,34 @@ public class AttendanceController
     @ActionTrigger("selectionAction")
     private JFXButton selectionButton;
 
+    @FXML
+    @ActionTrigger("submitAction")
+    private JFXButton submitButton;
+
+    @FXML private SimpleBooleanProperty mondayChecked = new SimpleBooleanProperty(false);
+    @FXML private SimpleBooleanProperty tuesdayChecked = new SimpleBooleanProperty(false);
+    @FXML private SimpleBooleanProperty wednesdayChecked = new SimpleBooleanProperty(false);
+    @FXML private SimpleBooleanProperty thursdayChecked = new SimpleBooleanProperty(false);
+    @FXML private SimpleBooleanProperty fridayChecked = new SimpleBooleanProperty(false);
+
+    private ObservableList<Student> data;
+
+    private Connection connection;
+    private Statement statement;
+
+    private String[] days;
+    private String selection;
+    private ArrayList<Integer> ids;
+
     @PostConstruct
     public void init()
     {
         Properties properties = new Properties();
-        Connection connection = null;
-        Statement statement = null;
 
         ArrayList<String> classes = new ArrayList<>();
 
-        try {
+        try
+        {
             properties.load(new FileInputStream(".//src//database.properties"));
             String url = properties.getProperty("jdbc.url");
             Class.forName(properties.getProperty("jdbc.driver"));
@@ -79,100 +95,59 @@ public class AttendanceController
             System.err.printf("problem with driver: %s\n", msg);
         }
 
-        try {
-            int logins = 0;
+        Calendar now = Calendar.getInstance();
 
-            properties.load(new FileInputStream(".//src//database.properties"));
-            String url = properties.getProperty("jdbc.url");
-            Class.forName(properties.getProperty("jdbc.driver"));
-            connection = DriverManager.getConnection(url, properties.getProperty("jdbc.username"), properties.getProperty("jdbc.password"));
+        SimpleDateFormat format = new SimpleDateFormat("MM/dd/yyyy");
 
-            statement = connection.createStatement();
+        int delta = -now.get(GregorianCalendar.DAY_OF_WEEK) + 2; //add 2 if your week start on monday
+        now.add(Calendar.DAY_OF_MONTH, delta);
 
-            ResultSet resultSet = statement.executeQuery("select logins from users where userID = " + 1001);
+        days = new String[5];
 
-            while (resultSet.next())
-            {
-                logins = resultSet.getInt(1);
-            }
-
-            Calendar now = Calendar.getInstance();
-
-            SimpleDateFormat format = new SimpleDateFormat("MM/dd/yyyy");
-
-            String[] days = new String[5];
-            int delta = -now.get(GregorianCalendar.DAY_OF_WEEK) + 2; //add 2 if your week start on monday
-            now.add(Calendar.DAY_OF_MONTH, delta);
-
-            for (int i = 0; i < 5; i++)
-            {
-                days[i] = format.format(now.getTime());
-                now.add(Calendar.DAY_OF_MONTH, 1);
-            }
-
-            for(int i = 0; i < days.length; i++)
-            {
-                if(i == 0)
-                {
-                    monday.setText(days[i]);
-                }
-                else if(i == 1)
-                {
-                    tuesday.setText(days[i]);
-                }
-                else if(i == 2)
-                {
-                    wednesday.setText(days[i]);
-                }
-                else if(i == 3)
-                {
-                    thursday.setText(days[i]);
-                }
-                else if(i == 4)
-                {
-                    friday.setText(days[i]);
-                }
-            }
-
-        }
-        catch (IOException ioException)
+        for (int i = 0; i < 5; i++)
         {
-            String msg = ioException.getMessage();
-            System.err.printf("problem with properties file: %s\n", msg);
+            days[i] = format.format(now.getTime());
+            now.add(Calendar.DAY_OF_MONTH, 1);
         }
-        catch (SQLException sqlException)
+
+        for (int i = 0; i < days.length; i++)
         {
-            String msg = sqlException.getMessage();
-            System.err.printf("problem with db connection: %s\n", msg);
-        }
-        catch (ClassNotFoundException e)
-        {
-            String msg = e.getMessage();
-            System.err.printf("problem with driver: %s\n", msg);
+            if (i == 0)
+            {
+                monday.setText(days[i]);
+            }
+            else if (i == 1)
+            {
+                tuesday.setText(days[i]);
+            }
+            else if (i == 2)
+            {
+                wednesday.setText(days[i]);
+            }
+            else if (i == 3)
+            {
+                thursday.setText(days[i]);
+            }
+            else if (i == 4)
+            {
+                friday.setText(days[i]);
+            }
         }
     }
 
     @ActionMethod("selectionAction")
     public void selectionButton_onAction() throws Exception
     {
-        ObservableList<Student> data = rosterView.getItems();
+        data = rosterView.getItems();
         data.clear();
-        Properties properties = new Properties();
-        Connection connection = null;
-        Statement statement = null;
 
-        ArrayList<Integer> ids = new ArrayList<>();
+        ids = new ArrayList<>();
+        ArrayList<String> rset = new ArrayList<>();
 
-        String selection = classCB.getSelectionModel().getSelectedItem();
+        selection = classCB.getSelectionModel().getSelectedItem();
 
-        try {
-            properties.load(new FileInputStream(".//src//database.properties"));
-            String url = properties.getProperty("jdbc.url");
-            Class.forName(properties.getProperty("jdbc.driver"));
-            connection = DriverManager.getConnection(url, properties.getProperty("jdbc.username"), properties.getProperty("jdbc.password"));
-
-            statement = connection.createStatement();
-
+        try
+        {
             ResultSet resultSet = statement.executeQuery("select studentID from rosters where courseID in (select courseID from courses where courseName = '" + selection + "')");
 
             while (resultSet.next())
@@ -186,25 +161,155 @@ public class AttendanceController
 
                 while (resultSet.next())
                 {
-                    String name = resultSet.getString(1);
+                    String name = resultSet.getString(1) + " " + resultSet.getString(2);
                     data.add(new Student(name));
                 }
             }
-        }
-        catch (IOException ioException)
-        {
-            String msg = ioException.getMessage();
-            System.err.printf("problem with properties file: %s\n", msg);
+
+            String[][] value = new String[ids.size()][5];
+
+            for (String day : days)
+            {
+                resultSet = statement.executeQuery("select `" + day + "` from " + selection + "Attendance");
+
+                while (resultSet.next())
+                {
+                    rset.add(resultSet.getString(1));
+                }
+            }
+
+            int count = 0;
+
+            for(int i = 0; i < days.length; i++)
+            {
+                for(int j = 0; j < value.length; j++)
+                {
+                    value[j][i] = rset.get(count);
+                    count++;
+                }
+            }
+
+            for(int i = 0; i < days.length; i++)
+            {
+                count = 0;
+
+                for (String[] aValue : value)
+                {
+                    switch(i)
+                    {
+                        case 0:
+                            if(aValue[i].equals("Y"))
+                            {
+                                data.get(count).setMondayChecked(true);
+                            }
+                            break;
+                        case 1:
+                            if(aValue[i].equals("Y"))
+                            {
+                                data.get(count).setTuesdayChecked(true);
+                            }
+                            break;
+                        case 2:
+                            if(aValue[i].equals("Y"))
+                            {
+                                data.get(count).setWednesdayChecked(true);
+                            }
+                            break;
+                        case 3:
+                            if(aValue[i].equals("Y"))
+                            {
+                                data.get(count).setThursdayChecked(true);
+                            }
+                            break;
+                        case 4:
+                            if(aValue[i].equals("Y"))
+                            {
+                                data.get(count).setFridayChecked(true);
+                            }
+                            break;
+                        default:
+                            break;
+                    }
+                    count++;
+                }
+            }
         }
         catch (SQLException sqlException)
         {
             String msg = sqlException.getMessage();
             System.err.printf("problem with db connection: %s\n", msg);
         }
-        catch (ClassNotFoundException e)
+    }
+
+    @ActionMethod("submitAction")
+    public void submitButton_onAction() throws Exception
+    {
+        data = rosterView.getItems();
+
+        mondayChecked.addListener((ov, old_val, new_val) ->
         {
-            String msg = e.getMessage();
-            System.err.printf("problem with driver: %s\n", msg);
+            data.get(0).setMondayChecked(true);
+        });
+
+        tuesdayChecked.addListener((ov, old_val, new_val) ->
+        {
+            data.get(0).setTuesdayChecked(true);
+        });
+
+        wednesdayChecked.addListener((ov, old_val, new_val) ->
+        {
+            data.get(0).setWednesdayChecked(true);
+        });
+
+        thursdayChecked.addListener((ov, old_val, new_val) ->
+        {
+            data.get(0).setThursdayChecked(true);
+        });
+
+        fridayChecked.addListener((ov, old_val, new_val) ->
+        {
+            data.get(0).setFridayChecked(true);
+        });
+
+        for(int i = 0; i < data.size(); i++)
+        {
+            char[] check = {'N', 'N', 'N', 'N', 'N'};
+
+            if(data.get(i).getMonChecked())
+            {
+                check[0] = 'Y';
+            }
+            if (data.get(i).getTueChecked())
+            {
+                check[1] = 'Y';
+            }
+            if (data.get(i).getWedChecked())
+            {
+                check[2] = 'Y';
+            }
+            if (data.get(i).getThurChecked())
+            {
+                check[3] = 'Y';
+            }
+            if (data.get(i).getFriChecked())
+            {
+                check[4] = 'Y';
+            }
+
+            try
+            {
+                statement = connection.createStatement();
+
+                for(int j = 0; j < days.length; j++)
+                {
+                    statement.executeUpdate("UPDATE " + selection + "Attendance set `" + days[j] + "` = '" + check[j] + "' where studentID = " + ids.get(i));
+                }
+            }
+            catch (SQLException sqlException)
+            {
+                String msg = sqlException.getMessage();
+                System.err.printf("problem with db connection: %s\n", msg);
+            }
         }
     }
 }
